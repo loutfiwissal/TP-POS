faeacab5b2613afc6a8eadac2d694ea14dd13698
oumaima <oh.oumaima.hamza@gmail.com>
2025-10-18 22:46:44 +0100

    Add product entity for cafe project

---
+++ src/main/java/com/commerce/commerce/model/Category.java
@@ -0,0 +1,15 @@
+package com.commerce.commerce.model;
+
+public enum Category {
+    ESPRESSO,
+    LATTE,
+    CAPPUCCINO,
+    AMERICANO,
+    MOCHA,
+    MACCHIATO,
+    FRAPPE,
+    TEA,
+    ICED_COFFEE,
+    DECAF,
+    SPECIALTY
+}
--- src/main/java/com/commerce/commerce/model/Product.java
+++ src/main/java/com/commerce/commerce/model/Product.java
@@ -1,4 +1,49 @@
 package com.commerce.commerce.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "products")
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long idProduct;
+    @Column(nullable = false, length = 50)
+    private String name;
+    @Column(nullable = false)
+    private double price;
+    @Enumerated(EnumType.STRING)
+    private Category category;
+
+    public long getIdProduct() {
+        return idProduct;
+    }
+
+    public void setIdProduct(long idProduct) {
+        this.idProduct = idProduct;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
 }
